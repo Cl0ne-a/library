e68a94b04d18c00c7016ff55b41feb05324fe736
Anna <unit10@protonmail.com>
2021-12-28 16:36:06 +0200

    feat: added service

--- src/main/java/com/books/library/service/BookService.java
+++ src/main/java/com/books/library/service/BookService.java
@@ -18,10 +18,59 @@ public class BookService {
         this.bookDao = bookDao;
     }
 
-    // todo create
-//    public
-    // todo readAll
-    // todo readById
-    // todo update
-    // todo delete
+    public boolean createNewBook(Book book) {
+        boolean created = false;
+        int id = book.getId();
+        String title = book.getTitle();
+        int authorId = book.getAuthorId();
+        int genreId = book.getGenreId();
+        Book ifExists = bookDao.readById(id);
+        if (    ifExists!= null
+                && !ifExists.getTitle().equals(title)
+                && ifExists.getAuthorId() != authorId
+                && ifExists.getGenreId() != genreId) {
+            bookDao.create(book);
+            created = true;
+        }
+        return created;
+    }
+
+    public Map<String, List<String>> getAll() {
+        return bookDao.readAll();
+    }
+
+    public Book getById(int id) {
+        Book book = null;
+        if(bookDao.readById(id) == null) {
+            // todo handle exception
+            System.out.println("no such book available");
+        } else {
+            book = bookDao.readById(id);
+        }
+        return book;
+    }
+
+    public boolean updateBookName(int id, String title) {
+        boolean updated = false;
+
+        Book ifExists = bookDao.readById(id);
+
+        if (ifExists != null) {
+            bookDao.updateBook(id, title);
+            updated = true;
+        }
+        return updated;
+    }
+
+    public boolean deleteBookById(int id) {
+        boolean deleted = false;
+
+        Book ifExists = bookDao.readById(id);
+
+        if (ifExists != null) {
+            bookDao.deleteById(id);
+            deleted = true;
+        }
+        return deleted;
+    }
 }
